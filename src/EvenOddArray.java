a58d2a8f42bf0709d89e831fa734756efb31c943
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-11-20 11:06:13 +0530

    20 nov 2

---
+++ src/EvenOddArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EvenOddArray {
+    public static void evenOdd(int n,int[] arr){
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                System.out.print("Even ");
+            }
+            else{
+                System.out.print("odd ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the array length");
+        int n=sc.nextInt();
+        System.out.println("Enter the array element ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        evenOdd(n,arr);
+    }
+}
