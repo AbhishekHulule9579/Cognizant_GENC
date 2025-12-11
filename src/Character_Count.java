bcdfc3f70d90d974c90947862caecc8edc781048
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-11 19:46:52 +0530

    11 dec

--- src/Character_Count.java
+++ src/Character_Count.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Character_Count {
     public static int charCount(String str,char c){
         int count=0;
-        for(int i=0;i<str.length();i++){
+        for(int i=0;i<str.length();i++)
+        {
             if(str.charAt(i)==c){
                 count++;
             }
